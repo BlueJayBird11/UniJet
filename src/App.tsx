b9764f96dfca1e282e5de089e777be461e19340f
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-01-09 15:26:32 -0600

    Base with Vite React Ts

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className='app bg-gray-600'>
+      app
+    </div>
+  )
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "gray-20": "#F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCCC",
+        "gray-500": "#5E0000",
+        "primary-100": "#FFE1E0",
+        "primary-300": "#FFA6A3",
+        "primary-500": "#FF6B66",
+        "secondary-400": "#FFCD5B",
+        "secondary-500": "#FFC132",
+      },
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+}
