eee19bfff0e7b5daa76a67c2da3404940aa16a5e
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-01-09 16:36:33 -0600

    Began navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Profile from "@/scenes/profile";
+import NavigationBar from "@/scenes/navigationBar";
 
 function App() {
   return (
     <div className='app bg-gray-600'>
       <Profile />
+      <NavigationBar />
     </div>
   )
 }
---
+++ src/scenes/navigationBar/index.tsx
@@ -0,0 +1,43 @@
+import { CalendarDaysIcon, UserCircleIcon, ClockIcon, TruckIcon } from "@heroicons/react/24/solid";
+import { Console } from "console";
+import { motion } from "framer-motion";
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+    const handleButtonClick = () => {
+        console.log(`Clicked: {}`);
+    };
+
+    const buttonStyle = "bg-white rounded-full p-4 border-solid border-black"
+    const iconStyle = "h-10 w-10 text-black"
+    return (
+    <nav>
+        <div className='flex w-full items-center justify-between fixed bottom-0 py-2 bg-slate-300 px-6 gap-2'>
+            <button
+                className={`${buttonStyle}`}
+                onClick={handleButtonClick}
+            >
+                <CalendarDaysIcon className={`${iconStyle}`}/>
+            </button>
+            <button
+                className={`${buttonStyle}`}    
+            >
+                <TruckIcon className={`${iconStyle}`}/>
+            </button>
+            <button
+                className={`${buttonStyle}`}    
+            >
+                <ClockIcon className={`${iconStyle}`}/>
+            </button>
+            <button
+                className={`${buttonStyle}`}    
+            >
+                <UserCircleIcon className={`${iconStyle}`}/>
+            </button>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
