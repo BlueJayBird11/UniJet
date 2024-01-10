a18a8ef3ec50f56bd8f4b72b27a906330fb28192
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-01-09 18:01:49 -0600

    Routing Buttons Continued
    Error: Link element is having problems

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,28 @@ import FindRider from "@/scenes/findRider";
 import FindDriver from "@/scenes/findDriver";
 import History from "@/scenes/history";
 import Schedule from "@/scenes/schedule";
+import Settings from "@/scenes/settings";
 import { useState } from "react";
 import { SelectedPage } from "./shared/types";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Profile
   );
-  const Page = <Profile />;
+  const page = <Profile />;
   return (
     <div className='app bg-primary-blue'>
       <NavigationBar 
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
+        page = {page}
       />
+      
       <BrowserRouter>
         <Routes>
-          <Route index element={Page} />
+          <Route index element={page} />
           <Route path="@/scenes/profile" element={<Profile />}/>
           <Route path="@/scenes/history" element={<History />}/>
         </Routes>
--- src/scenes/navigationBar/index.tsx
+++ src/scenes/navigationBar/index.tsx
@@ -2,40 +2,47 @@ import { SelectedPage } from "@/shared/types";
 import { CalendarDaysIcon, UserCircleIcon, ClockIcon, TruckIcon } from "@heroicons/react/24/solid";
 import { Console } from "console";
 import { motion } from "framer-motion";
+import History from "@/scenes/history";
+import { Link } from "react-router-dom";
 
 type Props = {
     selectedPage: SelectedPage;
     setSelectedPage: (value: SelectedPage) => void;
+    page: JSX.Element;
 }
 
-const Navbar = ({selectedPage, setSelectedPage}: Props) => {
+const Navbar = ({selectedPage, setSelectedPage, page}: Props) => {
     const handleButtonClick = () => {
         console.log(`Clicked: {}`);
     };
-
+    const setPage = () => {
+        page = <History />
+    }
+    
     const buttonStyle = "rounded-full p-4 border-solid border-black"
     const iconStyle = "h-10 w-10 text-black"
     return (
     <nav>
         <div className='bg-primary-red flex w-full items-center justify-between fixed bottom-0 py-2 px-6 gap-2'>
-            <button
-                className={`${buttonStyle}`}
-                onClick={handleButtonClick}
-            >
+            <button className={`${buttonStyle}`} > 
+            {/* //onClick={() => setSelectedPage(SelectedPage.Schedule)}  */}
                 <CalendarDaysIcon className={`${iconStyle}`}/>
             </button>
             <button
-                className={`${buttonStyle}`}    
+                className={`${buttonStyle}`} 
+                onClick={() => setSelectedPage(SelectedPage.FindDriver)}   
             >
                 <TruckIcon className={`${iconStyle}`}/>
             </button>
             <button
                 className={`${buttonStyle}`}    
+                onClick={() => setSelectedPage(SelectedPage.History)}
             >
                 <ClockIcon className={`${iconStyle}`}/>
             </button>
             <button
                 className={`${buttonStyle}`}    
+                onClick={() => setSelectedPage(SelectedPage.Profile)}
             >
                 <UserCircleIcon className={`${iconStyle}`}/>
             </button>
