37ba1d5f68c4b19174f5b3957a842a4b5a60aac5
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-01-10 16:41:40 -0600

    Made the base for the driver div

--- src/scenes/findDriver/index.tsx
+++ src/scenes/findDriver/index.tsx
@@ -1,4 +1,4 @@
-import { AdjustmentsHorizontalIcon, SparklesIcon } from "@heroicons/react/24/solid";
+import { AdjustmentsHorizontalIcon, SparklesIcon, UserCircleIcon, CheckCircleIcon } from "@heroicons/react/24/solid";
 
 type Props = {}
 
@@ -7,12 +7,21 @@ const FindDriver = (props: Props) => {
     console.log(`Clicked: {}`);
   };
   const iconStyle = "h-8 w-8 text-black";
-  const iconTextStyle = "flex items-center justify-between"
-  const textStyle = "text-black text-xs"
+  const iconTextStyle = "flex items-center justify-between";
+  const headerTextStyle = "text-black text-xs";
+  
+  const userIconStyle = "h-20 w-20 text-black";
+  const userTextStyle = "text-black text-xl font-bold";
+  const styleToCenterTxt = "flex items-center h-full pl-2"
+  const userInfoTextStyle = "text-black text-2xl text-left font-bold";
+
   return (
     <section>
+      {/* HEADER SECTION */}
       <div className="w-full py-6">
+        {/* ITEMS FOR HEADER */}
         <div className="flex items-center justify-between">
+          {/* FILTER BUTTON */}
           <button
             className="pl-4"
             onClick={filterButtonClickEvent}
@@ -20,23 +29,52 @@ const FindDriver = (props: Props) => {
             <div className={`${iconTextStyle}`}>
               <AdjustmentsHorizontalIcon className={`${iconStyle}`} />
               <div className="flex items-center">
-                <p className={`${textStyle} text-center`}>Filter</p>
+                <p className={`${headerTextStyle} text-center`}>Filter</p>
               </div>
             </div>
           </button>
+          {/* INPUT */}
           <input 
             title="driverSearch" 
             type="text" 
             placeholder="Search"  
           />
+          {/* OPTIMIZE BUTTON */}
           <button className="pr-4">
             <div className={`${iconTextStyle}`}>
               <SparklesIcon className={`${iconStyle}`} />
               <div className="flex items-center">
-                <p className={`${textStyle} text-center`}>Optimize</p>
+                <p className={`${headerTextStyle} text-center`}>Optimize</p>
               </div>
             </div>
           </button>
+        </div>
+      </div>
+
+      {/* PEOPLE */}
+      <div className="px-2">
+        <div className="bg-gray-600 rounded-[20px] px-2 py-4">
+          <div className="flex items-center justify-between px-2">
+            {/* PROFILE PICTURE */}
+            <UserCircleIcon className={`${userIconStyle}`}/>
+            {/* NAME AND RATING */}
+            <div className="pr-16">
+              <p className={`${userTextStyle}`}>Ash</p>
+              <p className={`${userTextStyle}`}>Rating: 5.0 Stars</p>
+            </div>
+          </div>
+          <div className={`${styleToCenterTxt}`}>
+            <p className={`${userInfoTextStyle}`}>Distance away: 1.5 mi</p>
+            {/* <p className={`${userInfoTextStyle}`}>Distance away: 1.5 mi <br/>Drop off: Nethken <br/>Pay: $9-$11</p> */}
+            {/* <p className={`${userInfoTextStyle}`}></p>
+            <p className={`${userInfoTextStyle}`}></p> */}
+          </div>
+          <div className={`${styleToCenterTxt}`}>
+            <p className={`${userInfoTextStyle}`}>Drop off: Nethken</p>
+          </div>
+          <div className={`${styleToCenterTxt}`}>
+            <p className={`${userInfoTextStyle}`}>Pay: $9-$11</p>
+          </div>
         </div>
       </div>
     </section>
