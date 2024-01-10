1bb8eff52972ed55bc48419201e2c706c35696e0
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-01-10 16:04:15 -0600

    Fixed formatting for buttons and added placeholder text for input

--- src/scenes/findDriver/index.tsx
+++ src/scenes/findDriver/index.tsx
@@ -3,22 +3,40 @@ import { AdjustmentsHorizontalIcon, SparklesIcon } from "@heroicons/react/24/sol
 type Props = {}
 
 const FindDriver = (props: Props) => {
+  const filterButtonClickEvent = () => {
+    console.log(`Clicked: {}`);
+  };
   const iconStyle = "h-8 w-8 text-black";
-  const iconTextStyle = "flex"
+  const iconTextStyle = "flex items-center justify-between"
   const textStyle = "text-black text-xs"
   return (
     <section>
-      <div className="w-full py-2">
-        <div className="flex justify-between">
-          <div className={`${iconTextStyle}`}>
-            <AdjustmentsHorizontalIcon className={`${iconStyle}`} />
-            <p className={`${textStyle} align-middle`}>Filter</p>
-          </div>
-          <input title="driverSearch" type="text" />
-          <div className={`${iconTextStyle}`}>
-            <SparklesIcon className={`${iconStyle}`} />
-            <p className={`${textStyle}`}>Optimize</p>
-          </div>
+      <div className="w-full py-6">
+        <div className="flex items-center justify-between">
+          <button
+            className="pl-4"
+            onClick={filterButtonClickEvent}
+          >
+            <div className={`${iconTextStyle}`}>
+              <AdjustmentsHorizontalIcon className={`${iconStyle}`} />
+              <div className="flex items-center">
+                <p className={`${textStyle} text-center`}>Filter</p>
+              </div>
+            </div>
+          </button>
+          <input 
+            title="driverSearch" 
+            type="text" 
+            placeholder="Search"  
+          />
+          <button className="pr-4">
+            <div className={`${iconTextStyle}`}>
+              <SparklesIcon className={`${iconStyle}`} />
+              <div className="flex items-center">
+                <p className={`${textStyle} text-center`}>Optimize</p>
+              </div>
+            </div>
+          </button>
         </div>
       </div>
     </section>
