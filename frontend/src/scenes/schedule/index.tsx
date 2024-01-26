d3d265ace9b8f3b0336d2857cfcebc49656c9840
eespeed <itseverydaybro263@gmail.com>
2024-01-26 00:38:17 -0600

    added scrollable component with 7am-11pm to scheduler page
    - made changes to schedule\index.tsx
    
    This is phase 1 of the scheduler component implementation.

--- frontend/src/scenes/schedule/index.tsx
+++ frontend/src/scenes/schedule/index.tsx
@@ -1,21 +1,30 @@
 import React from 'react'
-import Calendar from 'react-calendar'
 
 type Props = {}
 
 const Schedule = (props: Props) => {
-  return (
-    <div className="flex flex-col h-screen">
-      <div className="bg-primary-blue py-20 flex justify-center items-start">
-          <h1 className='text-5xl font-bold text-primary-black mt-6'> 
-            Scheduler
-          </h1>
-      </div>
-      <div className="flex-grow text-2xl bg-primary-white p-8">
-          <Calendar />
-      </div>
-      <div className="bg-primary-blue py-30">
+  
+  const generateHourLabels = () => {
+    const hours = [];
+    for (let i = 7; i <= 23; i++) {
+      const label = i <= 12 ? `${i} am` : `${i - 12} pm`;
+      hours.push(<div key={i} className="h-12 flex items-center justify-center">{label}</div>);
+    }
+    return hours;
+  };
 
+
+  return (
+    <div className="flex flex-col h-screen pb-24 bg-gray-600">
+      <div className="bg-primary-blue py-20 flex justify-center items-start">
+      </div>
+      <div className="flex-grow overflow-y-auto flex">
+        {/* Left column */}
+        <div className="w-16 bg-gray-600">
+          <div className="grid grid-rows-24 gap-20">
+            {generateHourLabels()}
+          </div>
+        </div>
       </div>
     </div>
   )
