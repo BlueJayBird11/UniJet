c85d7a70350eb384c7be01a574796f16928db41a
eespeed <itseverydaybro263@gmail.com>
2024-01-29 11:40:38 -0600

    Added weekday label
    only changed index.tsx of the schedule directory

--- frontend/src/scenes/schedule/index.tsx
+++ frontend/src/scenes/schedule/index.tsx
@@ -3,20 +3,35 @@ import React from 'react'
 type Props = {}
 
 const Schedule = (props: Props) => {
-  
-  const generateHourLabels = () => {
+    const generateHourLabels = () => {
     const hours = [];
     for (let i = 7; i <= 23; i++) {
       const label = i <= 12 ? `${i} am` : `${i - 12} pm`;
-      hours.push(<div key={i} className="h-12 flex items-center justify-center">{label}</div>);
+      hours.push(
+        <div key={i} className="h-12 flex items-center justify-center">
+          {label}
+        </div>
+      )
     }
-    return hours;
-  };
-
+    return hours
+  }
+  
+  const daysOfWeek = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
   return (
     <div className="flex flex-col h-screen pb-24 bg-gray-600">
-      <div className="bg-primary-blue py-20 flex justify-center items-start">
+      <div className="bg-primary-blue py-20 flex justify-center items-start relative">
+        {/* Gray bar behind weekday text */}
+        <div className="absolute py-6 m-10 bg-gray-400 w-full">
+        </div>
+        {/* Text in the blue area */}
+        <div className="flex justify-between w-full px-16 absolute py-10">
+            {daysOfWeek.map((day, index) => (
+              <div key={index} className="text-white m-3">
+                {day}
+              </div>
+            ))}
+        </div>
       </div>
       <div className="flex-grow overflow-y-auto flex">
         {/* Left column */}
