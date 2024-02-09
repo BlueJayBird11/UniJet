d69082ae03ca0bebe38165e8d4fa2265f5ad0a3d
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-02-08 23:30:49 -0600

    Updated Passengers.ts

--- backend_ts/src/model/Passengers.ts
+++ backend_ts/src/model/Passengers.ts
@@ -1,5 +1,83 @@
-// import { Column,Model,Table,DataType } from "sequelize-typescript";
+import {
+  Column,
+  Model,
+  Table,
+  DataType,
+  PrimaryKey,
+  AutoIncrement,
+} from "sequelize-typescript";
 
+@Table({
+  tableName: "passengers"
+}) //   timestamps: false, // Assuming you don't have created_at and updated_at columns
+export class Passenger extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.BIGINT)
+  id!: number;
+
+  @Column({
+    type: DataType.DATE,
+    allowNull: false,
+  })
+  birthDate!: Date;
+
+  @Column({
+    type: DataType.STRING(30),
+    allowNull: false,
+  })
+  email!: string;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  passwordHash!: number;
+
+  @Column({
+    type: DataType.CHAR(10),
+    allowNull: false,
+  })
+  phoneNumber!: string;
+
+  @Column({
+    type: DataType.STRING(25),
+    allowNull: false,
+  })
+  firstName!: string;
+
+  @Column({
+    type: DataType.STRING(40),
+    allowNull: false,
+  })
+  lastName!: string;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  userStatus!: number;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+  })
+  carPool!: boolean;
+
+  @Column({
+    type: DataType.FLOAT,
+    allowNull: true,
+  })
+  rating?: number;
+
+  @Column({
+    type: DataType.JSONB,
+    allowNull: true,
+  })
+  schedule?: object;
+}
+
+export default Passenger;
 
 // @Table({
 //     tableName: Passenger.PASSENGER_TABLE_NAME
@@ -14,7 +92,7 @@
 //     @Column({
 //         type: DataType.BIGINT,
 //         primaryKey: true,
-//         aut
+
 //     })
 //     id!:number
 // }
