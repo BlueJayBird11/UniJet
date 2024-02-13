98a3f9497f4578040ea00f1bbf04b870b03157e7
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-02-12 22:49:50 -0600

    Created Passenger Repo and Controller

---
+++ backend_ts/src/controller/PassengerController.ts
@@ -0,0 +1,116 @@
+import { Request, Response } from "express";
+import Passenger from "../model/Passenger";
+import { PassengerRepo } from "../repository/PassengerRepo";
+
+class PassengerController {
+  async create(req: Request, res: Response) {
+    try {
+      const new_passenger = new Passenger();
+      new_passenger.birthDate = req.body.birthDate;
+      new_passenger.email = req.body.email;
+      new_passenger.passwordHash = req.body.passwordHash;
+      new_passenger.phoneNumber = req.body.phoneNumber;
+      new_passenger.firstName = req.body.firstName;
+      new_passenger.lastName = req.body.lastName;
+      new_passenger.userStatus = req.body.userStatus;
+      new_passenger.carPool = req.body.carPool;
+      new_passenger.rating = req.body.rating;
+      new_passenger.schedule = req.body.schedule;
+
+      await new PassengerRepo().save(new_passenger);
+
+      res.status(201).json({
+        status: "Created!",
+        message: "Successfully created passenger!",
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "Internal Server Error.",
+        message: "Internal Server Error.",
+      });
+    }
+  }
+
+  async delete(req: Request, res: Response) {
+    try {
+      let id = parseInt(req.params["id"]);
+      await new PassengerRepo().delete(id);
+
+      res.status(200).json({
+        status: "OK!",
+        message: "Successfully deleted passenger!",
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "Internal Server Error.",
+        message: "Internal Server Error.",
+      });
+    }
+  }
+
+  async findById(req: Request, res: Response) {
+    try {
+      let id = parseInt(req.params["id"]);
+      const new_passenger = await new PassengerRepo().retrieveById(id);
+
+      res.status(200).json({
+        status: "OK!",
+        message: "Successfully fetched passenger by id!",
+        data: new_passenger,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "Internal Server Error.",
+        message: "Internal Server Error.",
+      });
+    }
+  }
+
+  async findAll(req: Request, res: Response) {
+    try {
+      const new_passenger = await new PassengerRepo().retrieveAll();
+
+      res.status(200).json({
+        status: "OK!",
+        message: "Successfully fetched all passengers data!",
+        data: new_passenger,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "Internal Server Error.",
+        message: "Internal Server Error.",
+      });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    try {
+      let id = parseInt(req.params["id"]);
+      const new_passenger = new Passenger();
+
+      new_passenger.id = id;
+      new_passenger.birthDate = req.body.birthDate;
+      new_passenger.email = req.body.email;
+      new_passenger.passwordHash = req.body.passwordHash;
+      new_passenger.phoneNumber = req.body.phoneNumber;
+      new_passenger.firstName = req.body.firstName;
+      new_passenger.lastName = req.body.lastName;
+      new_passenger.userStatus = req.body.userStatus;
+      new_passenger.carPool = req.body.carPool;
+      new_passenger.rating = req.body.rating;
+      new_passenger.schedule = req.body.schedule;
+
+      await new PassengerRepo().update(new_passenger);
+
+      res.status(200).json({
+        status: "OK!",
+        message: "Successfully updated passenger data!",
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "Internal Server Error.",
+        message: "Internal Server Error.",
+      });
+    }
+  }
+}
--- backend_ts/src/repository/PassengerRepo.ts
+++ backend_ts/src/repository/PassengerRepo.ts
@@ -1,5 +1,94 @@
 import { Passenger } from "../model/Passenger";
 
 interface IPassengerRepo {
-    create(passenger: Passenger): Promise<void>;
+  save(passenger: Passenger): Promise<void>;
+  update(passenger: Passenger): Promise<void>;
+  delete(passengerId: number): Promise<void>;
+  retrieveById(passengerId: number): Promise<Passenger>;
+  retrieveAll(): Promise<Passenger[]>;
+}
+
+export class PassengerRepo implements IPassengerRepo {
+  async save(passenger: Passenger): Promise<void> {
+    try {
+      await Passenger.create({
+        birthDate: passenger.birthDate,
+        email: passenger.email,
+        passwordHash: passenger.passwordHash,
+        phoneNumber: passenger.phoneNumber,
+        firstName: passenger.firstName,
+        lastName: passenger.lastName,
+        userStatus: 0,
+        carPool: false,
+        rating: null,
+        schedule: null,
+      });
+    } catch (error) {
+      throw new Error("Failed to create passenger.");
+    }
+  }
+  async update(passenger: Passenger): Promise<void> {
+    try {
+      const new_passenger = await Passenger.findOne({
+        where: {
+          id: passenger.id,
+        },
+      });
+      if (!new_passenger) {
+        throw new Error("Passenger not found.");
+      }
+      new_passenger.birthDate = passenger.birthDate;
+      new_passenger.email = passenger.email;
+      new_passenger.passwordHash = passenger.passwordHash;
+      new_passenger.phoneNumber = passenger.phoneNumber;
+      new_passenger.firstName = passenger.firstName;
+      new_passenger.lastName = passenger.lastName;
+      new_passenger.userStatus = passenger.userStatus;
+      new_passenger.carPool = passenger.carPool;
+      new_passenger.rating = passenger.rating;
+      new_passenger.schedule = passenger.schedule;
+
+      await new_passenger.save();
+    } catch (error) {
+      throw new Error("Failed to update passenger.");
+    }
+  }
+  async delete(passengerId: number): Promise<void> {
+    try {
+      const new_passenger = await Passenger.findOne({
+        where: {
+          id: passengerId,
+        },
+      });
+      if (!new_passenger) {
+        throw new Error("Passenger not found.");
+      }
+
+      await new_passenger.destroy();
+    } catch (error) {
+      throw new Error("Failed to delete passenger.");
+    }
+  }
+  async retrieveById(passengerId: number): Promise<Passenger> {
+    try {
+      const new_passenger = await Passenger.findOne({
+        where: {
+          id: passengerId,
+        },
+      });
+      if (!new_passenger) {
+        throw new Error("Passenger not found.");
+      }
+      return new_passenger;
+    } catch (error) {
+      throw new Error("Failed to retrieve passenger.");
+    }
+  }
+  async retrieveAll(): Promise<Passenger[]> {
+    try {
+      return await Passenger.findAll();
+    } catch (error) {
+      throw new Error("Failed to retrive passengers.");
+    }
+  }
 }
