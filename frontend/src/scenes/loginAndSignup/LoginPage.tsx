6a0b2460e5dbd3fbb8cece06956c089c0c985cf4
Samman Regmi <samman.regmi@gmail.com>
2024-02-24 16:39:50 -0600

    user get proper invalid warning when login fails

--- frontend/src/scenes/loginAndSignup/LoginPage.tsx
+++ frontend/src/scenes/loginAndSignup/LoginPage.tsx
@@ -20,21 +20,32 @@ const LoginPage: React.FC<LoginPageProps> = ({ onLogin }) => {
     e.preventDefault();
     setLoginError('');
     setEmailVerificationWarning('');
-
+  
     try {
       const userCredential = await signInWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
-
+  
       if (!user.emailVerified) {
         setEmailVerificationWarning('Please verify your email before logging in.');
         return;
+      } else{
+        onLogin();
+        navigate('/dashboard');
       }
-
-      console.log(email, password);
-      onLogin();
-      navigate('/dashboard'); // Navigate to the dashboard or the appropriate page
+  
     } catch (error: any) {
-      setLoginError(error.message);
+      console.log(error.code);
+      switch (error.code) {
+        case 'auth/user-not-found':
+          setLoginError('No account found with this email address.');
+          break;
+        case 'auth/invalid-credential':
+          setLoginError('Provided credentials are invalid. Please try again.');
+          break;
+        default:
+          setLoginError('Login failed. Please try again.');
+          break;
+      }
     }
   };
 
@@ -44,6 +55,18 @@ const LoginPage: React.FC<LoginPageProps> = ({ onLogin }) => {
         <h1 className="text-6xl font-bold text-center mb-2 text-white shadow-lg bg-opacity-50 bg-black px-3 py-1 rounded">UNIJET</h1>
         <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <h2 className="text-xl font-semibold text-center mb-6">Login</h2>
+
+          {emailVerificationWarning && (
+            <p className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4" role="alert">
+              {emailVerificationWarning}
+            </p>
+          )}
+
+          {loginError && (
+            <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4">{loginError}</div>
+          )}
+
+
           <form onSubmit={handleLogin} className="space-y-4">
             <input 
               type="email"
