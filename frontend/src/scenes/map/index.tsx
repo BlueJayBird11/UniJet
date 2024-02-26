735c6302d1fd27230cf15fd1093fec87703a3027
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-02-25 19:33:42 -0600

    Fixed formatting for map

--- frontend/src/scenes/map/index.tsx
+++ frontend/src/scenes/map/index.tsx
@@ -20,7 +20,7 @@ const Map: React.FC = () => {
 
   return (
     <div className="h-screen">
-      <MapContainer style={{ width: '100%', height: '86.6%' }} center={position} zoom={13} scrollWheelZoom={true}>
+      <MapContainer style={{ width: '100%', height: '90.5%' }} center={position} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
